a364667f5d7df56d3e0d3a7d6df0a4a11cd20771
loosbrecht <loos.brecht@gmail.com>
2021-12-09 20:52:09 +0100

    day9 part 2 Recursion is the waaay to go!

--- src/day9/Day9.kt
+++ src/day9/Day9.kt
@@ -15,6 +15,96 @@ class Day9 : Days {
         return lowRiskPoints.sum().toString()
     }
 
+    override fun solvePart2(): String {
+        val heightMap = readInputForDay().map { l -> l.split("").filterNot { it == "" }.map { Point(it, false) } }
+        val allBasinSizes = createBasinsFromMap(heightMap)
+        val size = allBasinSizes.sortedDescending().subList(0, 3).reduce { acc, i -> acc * i }
+        return size.toString()
+    }
+
+    private fun createBasinsFromMap(heightMap: List<List<Point>>): List<Int> {
+        val basinSize = mutableListOf<Int>()
+        heightMap.flatten().filter { it.s == "9" }.forEach { it.inBasin = true }
+        for (i in heightMap.indices) {
+            for (j in heightMap[i].indices) {
+                if (!heightMap[i][j].inBasin) {
+                    heightMap[i][j].inBasin = true
+                    basinSize += (1 + goAllDirs.sumOf { it(i, j, heightMap) })
+                }
+            }
+        }
+        return basinSize
+    }
+
+    private fun printMap(heightMap: List<List<Point>>) {
+        for (i in heightMap.indices) {
+            for (j in heightMap[i].indices) {
+                if (heightMap[i][j].inBasin) {
+                    print("s")
+                } else {
+                    print(heightMap[i][j].s)
+                }
+            }
+            println()
+        }
+        println()
+    }
+
+    private val goAllDirs = listOf(::goNorth, ::goSouth, ::goLeft, ::goRight)
+
+    private fun goNorth(i: Int, j: Int, heightMap: List<List<Point>>): Int {
+        val iNew = i - 1
+        val point = heightMap.getOrNull(iNew)?.getOrNull(j)
+        return if (point == null) {
+            0
+        } else if (point.inBasin) {
+            0
+        } else {
+            point.inBasin = true
+            1 + goAllDirs.sumOf { it(iNew, j, heightMap) }
+        }
+    }
+
+    private fun goSouth(i: Int, j: Int, heightMap: List<List<Point>>): Int {
+        val iNew = i + 1
+        val point = heightMap.getOrNull(iNew)?.getOrNull(j)
+        return if (point == null) {
+            0
+        } else if (point.inBasin) {
+            0
+        } else {
+            point.inBasin = true
+            1 + goAllDirs.sumOf { it(iNew, j, heightMap) }
+        }
+    }
+
+    private fun goLeft(i: Int, j: Int, heightMap: List<List<Point>>): Int {
+        val jNew = j - 1
+        val point = heightMap.getOrNull(i)?.getOrNull(jNew)
+        return if (point == null) {
+            0
+        } else if (point.inBasin) {
+            0
+        } else {
+            point.inBasin = true
+            1 + goAllDirs.sumOf { it(i, jNew, heightMap) }
+        }
+    }
+
+    private fun goRight(i: Int, j: Int, heightMap: List<List<Point>>): Int {
+        val jNew = j + 1
+        val point = heightMap.getOrNull(i)?.getOrNull(jNew)
+        return if (point == null) {
+            0
+        } else if (point.inBasin) {
+            0
+        } else {
+            point.inBasin = true
+            1 + goAllDirs.sumOf { it(i, jNew, heightMap) }
+        }
+    }
+
+
     private fun findLowPoints(heightMap: List<List<Int>>): List<Int> {
         val lowPoints = mutableListOf<Int>()
         val neighbours =
@@ -55,11 +145,9 @@ class Day9 : Days {
     }
 
 
-    override fun solvePart2(): String {
-        TODO("Not yet implemented")
-    }
-
     private fun parseInput(input: List<String>): List<List<Int>> {
         return input.map { l -> l.split("").filterNot { it == "" }.map { it.toInt() } }
     }
+
+    data class Point(val s: String, var inBasin: Boolean)
 }
